c9445b31c37306a30f6de485e78aa52fe6d86ba9
RyanBrim12 <12rab@optonline.net>
2026-07-29 04:16:27 -0400

    Stopped logging all queries

--- lib/database.ts
+++ lib/database.ts
@@ -18,7 +18,7 @@ if (!fs.existsSync(DB_DIR)) {
   fs.mkdirSync(DB_DIR, { recursive: true });
 }
 
-const db = new Database(DB_PATH, { verbose: console.log });
+const db = new Database(DB_PATH);
 
 db.pragma("journal_mode = WAL");
 db.pragma("foreign_keys = ON");
